f5942a4d73c01b5a97d8ee0d6e0b63415fdc27d5
s2oj7n <110683464+s2oj7n@users.noreply.github.com>
2023-06-11 13:09:50 +0900

    [Bronze V] Title: 개, Time: 128 ms, Memory: 14040 KB -BaekjoonHub

---
+++ 백준/Bronze/10172. 개/개.java
@@ -0,0 +1,24 @@
+
+
+import java.util.Scanner;
+
+
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("|\\_/|");
+		System.out.println("|q p|   /}");
+		System.out.println("( 0 )\"\"\"\\");
+		System.out.println("|\"^\"`    |");
+		System.out.println("||_/=\\\\__|");
+
+
+		
+		
+		
+
+		}
+
+}
